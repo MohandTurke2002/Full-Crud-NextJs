d488282e4bdce078f3141a10d8bb51641a6dbe75
mohandturky2002 <mohandhany35@gmail.com>
2023-05-28 04:52:02 +0300

    any

--- utils/dbConnect.js
+++ utils/dbConnect.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 
 const dbConnect = async () => {
   try {
-    await mongoose.connect(`${process.env.URL_CONNECT_DATA_BASE}`);
+    await mongoose.connect(
+      `mongodb+srv://${process.env.USER_NAME_DATA_BASE}:${process.env.PASSWORD_DATA_BASE}@cluster0.pk6jbgm.mongodb.net/Next-E-commerce`
+    );
     console.log("connected successfully");
   } catch (error) {
     console.log(error);
